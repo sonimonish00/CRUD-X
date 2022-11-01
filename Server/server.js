69488bfcee6b3ca1926e866c58f75dcedafacf8b
Monish Soni <sonimonish00@users.noreply.github.com>
2022-11-01 18:48:11 +0530

    testing env folder

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,14 @@
-console.log("Hello World!! This is a NodeJS Server !!")
+
+if(process.env.NODE_ENV=='production'){
+    console.log("This is production environment")
+}
+else if(process.env.NODE_ENV == 'development'){
+    console.log("This is development environment")
+}
+else {
+    console.log("I dont know which environment is this")
+}
+
+
+
+
