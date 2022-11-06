b18e2888602c5cc332e90e9978c7b0e293b5b528
Monish Soni <sonimonish00@users.noreply.github.com>
2022-11-07 01:58:22 +0530

    MongoDB Connection - Done !!

--- server/database/DBConn.js
+++ server/database/DBConn.js
@@ -1,33 +1,31 @@
 const mongoose = require("mongoose");
 require('dotenv').config({ path: 'env/.env' });
-
 const dev_db_url = 'local dev. db url is not defined.';
-console.log(process.env.MONGO_URL);
 const mongoDB_URL = process.env.MONGO_URL || dev_db_url;
 const dbOptions = {useNewUrlParser: true, useUnifiedTopology: true};
 
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// db.once("open", function () {
-//   console.log("Connected successfully");
-// });
-
-const connectDB = async (cb) => {
-  await mongoose
-    .connect(mongoDB_URL, dbOptions)
-    .then(() => {
-      cb();
-      console.log("Connected to Database");
-    })
-    .catch((err) => console.error("Could not Connect to Database", err));
+// MongoDB Connection Pattern 1 : Using without callbacks
+const connectDB = async () => {
+    try {
+     await mongoose.connect(mongoDB_URL, dbOptions);
+    } catch (error) {
+        console.error("Couldn't connect to MongoDB", error);
+        process.exit(1);
+    }
 };
 
-// (async () => {
+// MongoDB Connection Pattern 2 : Using callbacks
+// const connectDB = async (callback) => {
 //     try {
-//       await mongoose.connect(dbURI, dbOptions)
-//     } catch (err) {
-//       console.log('error: ' + err)
+//         await mongoose.connect(mongoDB_URL, dbOptions)
+//             .then(() => {
+//                 callback();
+//                 console.log("Connected to MongoDB");
+//             })
+//     } catch (error) {
+//         console.error("Couldn't connect to MongoDB", error);
+//         process.exit(1);
 //     }
-// })()
+// };
 
 module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config({ path: 'env/.env' });
+const mongoose = require("mongoose");
 const connectDB = require('./database/DBConn')
 const port = process.env.PORT || 5000;
 const express = require('express')
@@ -16,12 +17,20 @@ app.get('/', (req, res) => {
   }  
 })
 
-// Connecting to DB
-connectDB(()=>{
-  app.listen(port, () => {
-    console.log(`Backend : NodeJS/express server started on http://localhost:${port}`)
-  })
-});
+// MongoDB Connection Pattern 1 : Using events & w/o callbacks.
+connectDB();
+mongoose.connection
+  .on('connected',()=> console.log('Connected to MongoDB'))
+  .on('error',(error)=> console.log('MongoDB Error : ',error))
+  .on('disconnected',()=> console.log('Disconnected from MongoDB'))
+  .once('open', () => {app.listen(port, () => {console.log(`Backend : Node(express) server started listening on port : ${port}`)})})
+process.on('SIGINT', async()=>{await mongoose.connection.close(); process.exit(0)})
+
+// MongoDB Connection Pattern 2 : Using callbacks & w/o events.
+// connectDB(()=>{
+//     app.listen(port, () => {console.log(`Backend : Node(express) server started listening on port : ${port}`)})
+//   });
+
 
 
 
