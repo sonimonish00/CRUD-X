8c6a81d0032c4c25594317306b0d9ee48d98a0fa
Monish Soni <sonimonish00@users.noreply.github.com>
2022-12-11 22:34:15 +0530

    refactored

--- server/app/index.js
+++ server/app/index.js
@@ -1,13 +1,17 @@
 import express from "express";
+import { userRoutes } from "./routes/user.routes.js";
 const app = express();
 
-// Middlewares
+// Middlewares (Built-in + Custom)
 app.use(express.json());
 
 // We've 4 types of client roles : User, Admin, Manager, Salesman
+// Homepage URL : http://localhost:2000
+app.get("/", (req, res) => {
+  res.send("Welcome to homepage (Dashboard)");
+});
 // Routes -> REST API Endpoints (CRUD) : User Role
-import { userRoutes } from "./routes/user.routes.js";
-app.use("/users", userRoutes); // http://localhost:2000/users OR users/all
+app.use("/", userRoutes); // http://localhost:2000/users
 
 export { app };
 
--- server/app/routes/user.routes.js
+++ server/app/routes/user.routes.js
@@ -2,10 +2,8 @@ import express from "express";
 const router = express.Router();
 
 import { getAllUsers } from "../controllers/user.controllers.js";
-router.get("/", (req, res) => {
-  res.send("This is Default User API Path - without calling controller");
-});
-router.get("/all", getAllUsers);
+
+router.get("/users", getAllUsers);
 
 export { router as userRoutes };
 // OR export default router;
